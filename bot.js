8daebef86806802cbe18c16c5fa0df716b386623
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2019-06-05 16:42:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 if(!Discord) var Discord = require('discord.js');
 if(!client) var client = new Discord.Client();
-if(!prefix) var prefix = "A";
+if(!prefix) var prefix = "!";
 
 const adminprefix = "!";
 const devs = ['427611667631964180'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
-if (message.content.startsWith(adminprefix + 'pl')) {
+if (message.content.startsWith(adminprefix + 'play')) {
   client.user.setGame(argresult)
     message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
 } else 
@@ -15,12 +15,12 @@ if (message.content.startsWith(adminprefix + 'pl')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
   } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
+  if (message.content.startsWith(adminprefix + 'lis')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
   } else 
 if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/discordapp")
+  client.user.setGame(argresult, "https://www.twitch.tv/Allwai")
     message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
 }
 });
